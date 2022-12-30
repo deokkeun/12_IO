82e4416b5a61caa9cf22e62d0f24ed11e1f9ae62
kim deokkeun <deokk91@gmail.com>
2022-12-30 11:21:52 +0900

    12_IO

---
+++ src/edu/kh/io/Run/IORun.java
@@ -0,0 +1,16 @@
+package edu.kh.io.Run;
+
+import edu.kh.io.model.service.IOService;
+
+public class IORun {
+	public static void main(String[] args) {
+		IOService ioService = new IOService();
+		
+		//ioService.output1();
+		//ioService.input1();
+		//ioService.output2();
+		ioService.input2();
+		
+	}
+
+}
